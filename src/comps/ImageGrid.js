93fabe566c7f926180048374a1363f3180f0862b
Erni158 <ernest-kies@wp.pl>
2020-08-23 14:12:18 +0200

    Added animation

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
+import { motion } from 'framer-motion';
 
 const ImageGrid = ({ setSelectedImg }) => {
     const { docs } = useFirestore('images');
@@ -8,11 +9,17 @@ const ImageGrid = ({ setSelectedImg }) => {
     return(
         <div className="img-grid">
             { docs && docs.map(doc => (
-                <div className="img-wrap" key={doc.id}
+                <motion.div className="img-wrap" key={doc.id}
+                    layout
+                    whileHover={{ opacity: 1 }}
                     onClick={() => setSelectedImg(doc.url)}
                 >
-                    <img src={doc.url} alt="uploaded pic" />
-                </div>
+                    <motion.img src={doc.url} alt="uploaded pic" 
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 1 }}
+                    />
+                </motion.div>
             ))}
         </div>
     )
